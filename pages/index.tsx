66aefb0ca24a0699be9e5165d8956566f9fc2d16
Freddie Iboy <freddieiboy@gmail.com>
2022-12-17 10:33:09 +0100

    finish prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,34 +4,74 @@ import {
   ChangeEvent,
   ChangeEventHandler,
   HTMLInputTypeAttribute,
+  RefObject,
   useRef,
+  useEffect,
   useState,
 } from "react";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const size = useVisualViewportHeight();
+  const ogSize = useRef(size);
+
+  useEffect(() => {
+    ogSize.current = size;
+  }, []);
+
+  function handleOpen() {
+    setOpen(true);
+    ref.current?.focus();
+    console.log(window?.visualViewport?.height);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-navbutton-color" content="#ffffff" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff" />
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.navRow}>
+          <AppButton type={"cross"} />
+          <div className={styles.splitButton}>
+            <AppButton type={"link"} />
+            <AppButton type={"elipsis"} />
+          </div>
+        </div>
         <img className={styles.memeImg} src="meme.png" />
-        <div className={styles.card}>
+
+        {open ? (
+          <div ref={ref} className={styles.NewText} contentEditable>
+            Text
+          </div>
+        ) : null}
+
+        <div
+          ref={cardRef}
+          className={styles.card}
+          style={{
+            bottom: ogSize?.current - size,
+          }}
+        >
           <div className={styles.handle} />
           <div className={styles.navButtonRow}>
-            <NavButton type={"text"} />
+            <div onClick={handleOpen}>
+              <NavButton type={"text"} />
+            </div>
             <NavButton type={"stickers"} />
             <NavButton type={"layout"} />
             <NavButton type={"template"} />
             <NavButton type={"share"} />
           </div>
-
-          {/* <Input /> */}
         </div>
       </main>
     </div>
@@ -51,6 +91,18 @@ function NavButton({ type }: NavButtonProps) {
   );
 }
 
+type AppButtonProps = {
+  type: string;
+};
+
+function AppButton({ type }: AppButtonProps) {
+  return (
+    <button className={styles.appButton}>
+      <img src={`icon-${type}.svg`}></img>
+    </button>
+  );
+}
+
 function Input() {
   const ref = useRef(null);
   const [text, setText] = useState("");
@@ -61,10 +113,27 @@ function Input() {
 
   return (
     <input
+      className={styles.SearchBar}
       ref={ref}
       onChange={handleChange}
       placeholder="write text here..."
       value={text}
     />
   );
+}
+
+function useVisualViewportHeight() {
+  const [viewportHeight, setViewportHeight] = useState<number | undefined>(
+    undefined
+  );
+  useEffect(() => {
+    function handleResize() {
+      setViewportHeight(window?.visualViewport?.height);
+    }
+    window?.visualViewport?.addEventListener("resize", handleResize);
+    handleResize();
+    return () =>
+      window?.visualViewport?.removeEventListener("resize", handleResize);
+  }, []);
+  return viewportHeight;
 }
